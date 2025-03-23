1eaa5ac7b8954a2447ac3da27d516807cddd422c
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-03-23 15:06:08 +0530

    MainContent Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { BrowserRouter as Router } from "react-router";
+import { Route, BrowserRouter as Router, Routes } from "react-router";
 import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
 
 const App = () => {
   return (
     <Router>
       <div className="flex h-screen">
         <Sidebar />
+
+        <div className="rounded w-full flex justify-between flex-wrap">
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
