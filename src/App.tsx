0905d0c81ad7adfa94a10c5b9113f98609b426fb
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-03-24 15:38:44 +0530

    Project Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router";
-import Sidebar from "./components/Sidebar";
 import MainContent from "./components/MainContent";
+import ProductPage from "./components/ProductPage";
+import Sidebar from "./components/Sidebar";
+import TopSellers from "./components/TopSellers";
+import PopularBlogs from "./components/PopularBlogs";
 
 const App = () => {
   return (
@@ -8,10 +11,15 @@ const App = () => {
       <div className="flex h-screen">
         <Sidebar />
 
-        <div className="rounded w-full flex justify-between flex-wrap">
+        <div className="rounded w-full flex justify-center flex-wrap">
           <Routes>
             <Route path="/" element={<MainContent />} />
+            <Route path="/product/:id" element={<ProductPage />} />
           </Routes>
+          <div>
+            <TopSellers />
+            <PopularBlogs />
+          </div>
         </div>
       </div>
     </Router>
